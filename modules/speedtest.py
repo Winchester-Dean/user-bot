ad25070571b8e4fe030a1a9f8572c1e7f71fba57
Dean Winchester <110895086+Winchester-Dean@users.noreply.github.com>
2022-08-16 17:39:40 +0500

    Update speedtest.py

--- modules/speedtest.py
+++ modules/speedtest.py
@@ -11,6 +11,7 @@
 # You should have received a copy of the GNU General Public License along with this program.
 # If not, see <https://www.gnu.org/licenses/>.
 
+import speedtest
 import asyncio
 
 from speedtest import Speedtest
@@ -49,7 +50,7 @@ class SpeedTestModule(SessionConfig):
 
     @staticmethod
     def speedtester() -> Tuple[float, float, float]:
-        s = Speedtest()
+        s = speedtest.Speedtest()
 
         s.get_servers()
         s.get_best_server()
