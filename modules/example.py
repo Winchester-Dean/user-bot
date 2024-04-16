f18875626958d373e71398b72987bcaccfec5e6f
DBoy <xwftl909@gmail.com>
2024-04-16 22:01:00 +0500

    news

--- modules/example.py
+++ modules/example.py
@@ -1,8 +1,18 @@
-from session_config import SessionConfig
+from modules.help import HelpModule
 from telethon import events
 
-class ExampleModule(SessionConfig):
+class ExampleModule:
     """Example module; command: <code>.example</code>"""
+    def __init__(self):
+        self.commands = {
+            "name": "ExampleModule",
+            "descryption": "The module is intended as an example for creating other models for this userbot",
+            "commands": ".example"
+        }
+
+        self.help_module = HelpModule()
+        self.help_module.add_module_commands(self.commands)
+
     async def example_handler(self, msg):
         await msg.edit(
             "<b>Example</b>",
--- modules/help.py
+++ modules/help.py
@@ -1,18 +1,23 @@
 import os
 import inspect
+import utils
 
 from typing import List, Callable, Awaitable, Union
 from importlib import import_module
-from session_config import SessionConfig
 from telethon import events
 
+class HelpModule:
+    """View all modules list; command: <code>.help</code>"""
 
-class HelpModule(SessionConfig):
-    """View all modules; command: <code>.help</code>"""
-    def __init__(
-        self,
-        directory: str = "modules"
-    ):
+    strings = {
+        "error": "⚠ <b>Error:</b> <code>{error}</code>",
+        "no_module": "<b>Oops... the module was not found :(</b>",
+        "no_command": "<b>No commands found for this module...</b>",
+        "error_command": "⚠ <b>Error using the command</b>"
+    }
+
+    def __init__(self, directory: str = "modules"):
+        self.commands = {}
         self.all_modules: List[Union[Callable, Awaitable]] = []
         self.files = os.listdir(directory)
 
@@ -21,7 +26,7 @@ class HelpModule(SessionConfig):
                 file = file[:-3]
 
                 self.modules = import_module(
-                    f'{directory}.{file}'
+                    f"{directory}.{file}"
                 )
 
                 for classname, classobj in inspect.getmembers(
@@ -33,28 +38,67 @@ class HelpModule(SessionConfig):
                             classname,
                             classobj.__doc__
                         ))
-
+    
+    def add_module_commands(self, module_commands):
+        module_name = module_commands["name"]
+        self.commands[module_name] = {
+            "descryption": module_commands["descryption"],
+            "commands": module_commands["commands"]
+        }
+    
     async def help_handler(self, msg):
         try:
             text = (
                 "<b>💪 All modules:</b>\n\n"
             )
 
-            for index, module in enumerate(
-                self.all_modules
-            ):
-                name, doc = module
+            args = utils.get_args(msg)
+            if len(args) == 1:
+                for index, module in enumerate(
+                    self.all_modules, start=1
+                ):
+                    name, doc = module
+                    text += "<b>{}. {}: {}</b>".format(
+                        index, name, doc
+                    )
+            elif len(args) == 2:
+                module_name = args[1]
+                for module in self.all_modules:
+                    name, doc = module
+                    if module_name == name:
+                        if name in self.commands:
+                            commands_info = self.commands[name]["commands"]
+                            descryption = self.commands[name]["descryption"]
+                            
+                            text += "<b>Module: {}\nDescryption: {}\nCommands: {}\n</b>".format(
+                                name, descryption, commands
+                            )
 
-                text += "<b>{}. {}: {}</b> \n".format(
-                    index + 1,
-                    name,
-                    doc
+                            for command, info in commands_info.items():
+                                text += f"{command}: {info}\n"
+                        else:
+                            await msg.edit(
+                                self.strings["no_command"],
+                                parse_mode="html"
+                            )
+                    else:
+                        await msg.edit(
+                            self.strings["no_module"],
+                            parse_mode="html"
+                        )
+            else:
+                await msg.edit(
+                    self.strings["error_command"],
+                    parse_mode="html"
                 )
-
-            await msg.edit(text, parse_mode="html")
+            
+            await msg.edit(
+                text,
+                parse_mode="html"
+            )
         except Exception as error:
             await msg.edit(
-                f"⚠ <b>Error:</b> <code>{error}</code>",
+                self.strings["error"].format(error),
                 parse_mode="html"
             )
     
